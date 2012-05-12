f4ace3132c402461abbb23b2a033a5b3feefa372
Andrew Van Tassel <avantassel@gmail.com>
2012-05-11 22:10:27 -0600

    Added missing Error file, which was being referenced in Connection.php

---
+++ v1/lib/Error.php
@@ -0,0 +1,35 @@
+<?php 
+/** Error Class
+ *
+ * @package	mailhops
+ * @author  Andrew Van Tassel <andrew@andrewvantassel.com>
+ * @version	1.0
+ */
+class Error
+{
+
+	private static $error=null;
+	
+	public static function setError($error,$concat_errors=false){
+	if($concat_errors && !empty(self::$error))
+		self::$error.="\n".$error;
+	else
+		self::$error=$error;		
+	}
+	
+	public static function getError(){
+		return self::$error;
+	}
+	
+	public static function clearError(){
+		self::$error=null;
+	}
+	
+	public static function hasError(){
+		if(!empty(self::$error))
+			return true;
+		else
+			return false;
+	}
+}
+?>
